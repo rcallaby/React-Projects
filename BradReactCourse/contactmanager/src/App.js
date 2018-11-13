36ffa596bb020b225121a36aa834aa04273d22da
rcallaby <rcallaby@gmail.com>
2018-11-13 10:20:19 -0600

    Just added some more minor changes

--- BradReactCourse/contactmanager/src/App.js
+++ BradReactCourse/contactmanager/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Contacts from './components/Contacts';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
 
    return <div classname="app">
-       <h1>The Contact Manager App</h1>
+          <Header />
           <Contacts />
       </div>
   }
