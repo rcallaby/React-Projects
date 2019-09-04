5dc18a1eb5bad0b46b59f9283e092c42ad609361
rcallaby <rcallaby@gmail.com>
2019-09-04 18:32:52 -0500

    added key files

--- ReactFronttoBack2019/githubfinder/src/App.js
+++ ReactFronttoBack2019/githubfinder/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+   render() {
+     return(
     <div className="App">
       <h1>Hello from Richard</h1>
     </div>
-  );
+     )
+   }
+  ;
 }
 
 export default App;
