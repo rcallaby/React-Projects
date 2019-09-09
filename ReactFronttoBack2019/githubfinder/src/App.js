47bea4a7a9847ce72489271affaa995e48623bbe
rcallaby <rcallaby@gmail.com>
2019-09-09 10:21:11 -0500

    refactored project

--- ReactFronttoBack2019/githubfinder/src/App.js
+++ ReactFronttoBack2019/githubfinder/src/App.js
@@ -1,19 +1,38 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 import './App.css';
-import NavBar from './components/layout/NavBar.js';
-import Users from './components/users/Users.js';
 
-
-class App extends Component {
-   render() {
-     return(
-    <div className="App">
-      <NavBar></NavBar>
-      <Users></Users>
-    </div>
-     )
-   }
-  ;
-}
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
